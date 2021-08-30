31a8dd206283978ac21f93dbab00877de4574f84
Martin Ahoto <martinahoto4@gmail.com>
2021-08-30 13:39:27 +0300

    sum

--- sum of integers/main.cpp
+++ sum of integers/main.cpp
@@ -7,8 +7,15 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    int sum=0;
+    for(int i=1; i<=100;i++)
+    {
+        sum=sum+i;
+    }
+    cout<<"sum is "<<sum<<endl;
+    
     return 0;
 }
