8fe290928534ed56a9afc99cd88f55ce1fb107b7
Martin Ahoto <martinahoto4@gmail.com>
2021-08-30 13:35:57 +0300

    Initial Commit

---
+++ sum of integers/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sum of integers
+//
+//  Created by MARTIN on 30/08/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
